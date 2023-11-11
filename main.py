1a6ee7d1e0140d0261d9e984b60d9d88329ce042
SinghSahabbb <khyatisingh95118@gmail.com>
2023-11-11 18:17:14 +0530

    library commit

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn import preprocessing
 from sklearn import model_selection,neighbors
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,recall_score,roc_auc_score,confusion_matrix
 data = pd.read_csv(r"C:\Users\khyat\Downloads\kerala.csv")
 data.head()
 
@@ -49,9 +51,6 @@ knn_accuracy.mean()
 x_train_std = minmax.fit_transform(x_train)
 x_test_std = minmax.transform(x_test)
 
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LogisticRegression
-
 lr = LogisticRegression()
 lr_clf = lr.fit(x_train_std,y_train)
 y_predict = lr_clf.predict(x_test_std)
@@ -60,7 +59,6 @@ print(y_predict)
 print('Actual chances of flood')
 print(y_test.values)
 
-from sklearn.metrics import accuracy_score,recall_score,roc_auc_score,confusion_matrix
 print("\naccuracy score: %f"%(accuracy_score(y_test,y_predict)*100))
 print("recall score: %f"%(recall_score(y_test,y_predict)*100))
 print("roc score: %f"%(roc_auc_score(y_test,y_predict)*100))
